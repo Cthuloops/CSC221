32580bed3b72fab157ea4ea9842d85b414e11120
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2025-03-25 11:49:13 -0400

    feat(M5Pro): add basic structure for main

---
+++ Mod5/pro/m5_pro_coughlin.py
@@ -0,0 +1,54 @@
+"""This program interacts with a sqlite database"""
+# Menu driven program to access a sqlite database
+# 03/25/2025
+# CSC221 M5Pro - Database
+# Harley Coughlin
+
+
+def main():
+    """Entry point to the program."""
+    choice = 0
+    while choice != 6:
+        print_menu()
+        choice = get_menu_choice()
+
+        if choice == 1:
+            ...
+        elif choice == 2:
+            ...
+        elif choice == 3:
+            ...
+        elif choice == 4:
+            ...
+        elif choice == 5:
+            ...
+        elif choice == 6:
+            print("Thanks for using the program.")
+
+
+def print_menu():
+    """Prints the main menu."""
+    options = (
+        "1) Display OWNER content and create DataFrame",
+        "2) Display PETS content and create DataFrame",
+        "3) Retrieve Owner and Pet data for specific Owner",
+        "4) Calculate Total Charge by Owner",
+        "5) Retrieve pet information by PetBreed",
+        "6) Exit")
+    print(f"{'Main Menu':-^{{max(len(opt) for opt in options)}}}")
+    for opt in options:
+        print(opt)
+
+
+def get_menu_choice():
+    """Gets a menu option"""
+    try:
+        choice = int(input("Enter menu option: "))
+    except ValueError:
+        print("Please enter a valid number.")
+    else:
+        return choice
+
+
+if __name__ == "__main__":
+    main()
